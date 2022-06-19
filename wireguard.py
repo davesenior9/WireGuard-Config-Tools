dff98b94fd5381698ff2ea9d37f32d7690bb92ef
davesenior9 <94682387+davesenior9@users.noreply.github.com>
2022-06-19 11:24:57 +1000

    Add files via upload

--- wireguard.py
+++ wireguard.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os
 
 def key_generate(client_number):
@@ -8,7 +9,7 @@ def user_generate(client_number, description):
     with open(f'/etc/wireguard/clients/client{client_number}/privatekey', 'r') as f:
         private_key = f.read()
         private_key = str(private_key)
-    default_data = [(f'#Client{client_number} = {description}\n'),'[Interface]\n', (f'PrivateKey = {private_key}'), (f'Address = 10.0.0.{client_number}/32\n'), 'DNS = 10.0.0.1\n', '\n', '[Peer]\n','PublicKey = uJRgRh9Cj8TKtgSxkxGmB4ZeXwJE3kOxqkGnOaxSiAs=\n', 'AllowedIPs = 10.0.0.0/24, 192.168.8.0/24\n','Endpoint = vmsvpn.ddns.net:41194\n']
+    default_data = [(f'#Client{client_number} = {description}\n'),'[Interface]\n', (f'PrivateKey = {private_key}'), (f'Address = 192.168.100.{client_number}/32\n'), 'DNS = 1.1.1.1\n', '\n'>
     with open(f'/etc/wireguard/clients/client{client_number}/client.conf', 'w') as f:
         f.writelines(default_data)
         f.close
@@ -18,7 +19,7 @@ def server_side(client_number,description):
         public_key = f.read()
         public_key = str(public_key)
 
-    default_data = ['\n','[Peer]\n', (f'#{description}\n'), (f'AllowedIPs = 10.0.0.{client_number}/32\n'), (f'PublicKey = {public_key}\n')]
+    default_data = ['\n','[Peer]\n', (f'#{description}\n'), (f'AllowedIPs = 192.168.100.{client_number}/32\n'), (f'PublicKey = {public_key}\n')]
     with open(f'/etc/wireguard/wg0.conf', 'a') as f:
         f.writelines(default_data)
         f.close
@@ -29,25 +30,30 @@ def qr_code_generate():
 
 
 while True:
-    print ('\n')
-    cmd = os.system('ls clients')
-    print ('\n')
-    client_number = input('What is the Client Number?\n')
-    description = input('What is the Description of the Client?\n')
-    key_generate(client_number)
-    user_generate(client_number, description)
-    server_side(client_number, description)
-    while True:
-        qr_code = input('Do you want to generate a QR Code?(y/n)\n')
-        if qr_code == 'y':
-            qr_code_generate()
-            break
-        elif qr_code == 'n':
-            break
+    try:
+        print ('\n')
+        cmd = os.system('ls /etc/wireguard/clients')
+        print ('\n')
+        client_number = input('What is the Client Number?\n')
+        description = input('What is the Description of the Client?\n')
+        key_generate(client_number)
+        user_generate(client_number, description)
+        server_side(client_number, description)
+        while True:
+            qr_code = input('Do you want to generate a QR Code?(y/n)\n')
+            if qr_code == 'y':
+                qr_code_generate()
+                break
+            elif qr_code == 'n':
+                break
+            else:
+                print (f'You entered {qr_code}. Enter "y" or "n"\n')
+        restart = input('Do you want to make another Client?(y/n)\n')
+        if restart == 'y':
+            pass
         else:
-            print (f'You entered {qr_code}. Enter "y" or "n"\n')
-    restart = input('Do you want to make another Client?(y/n)\n')
-    if restart == 'y':
-        pass
-    else:
+            break
+    except KeyboardInterrupt:
+        print ('\nKeyboard Break')
         break
+
